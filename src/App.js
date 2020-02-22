d783733349ad6a64418f9bda7a0975643411caf7
Alexander Augusto <51683816+alexanderaugusto@users.noreply.github.com>
2020-02-22 15:55:18 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route } from "react-router-dom"
-import Pages from "./layout/Pages"
+import Pages from "./layout/pages"
 
 import { Provider } from 'react-redux'
 import store from "./redux/store"
